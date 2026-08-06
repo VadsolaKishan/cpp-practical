3c0f30c25f7194d14167cabf2edf57ac3e087334
VadsolaKishan <vadsolakishan1310@gmail.com>
2026-08-06 20:10:23 +0530

    Refactor Loan management code and remove comments
    Removed commented-out documentation and cleaned up code formatting.

--- Practical2_5.cpp
+++ Practical2_5.cpp
@@ -16,7 +16,6 @@ to include a feature to display comprehensive loan details, including the calcul
 friendly format. This functionality was intended to aid both customers and bank staff in managing 
 loan-related queries effectively. */
 
-
 #include<iostream>
 #include<math.h>
 using namespace std;
