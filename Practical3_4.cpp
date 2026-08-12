c0b06d91fe09e4e501a8e9cca53eca3d20d03f93
VadsolaKishan <vadsolakishan1310@gmail.com>
2026-08-12 15:49:13 +0530

    Refactor and clean up Practical3_4.cpp

--- Practical3_4.cpp
+++ Practical3_4.cpp
@@ -12,7 +12,6 @@ In practice, the system allowed for the creation of collections for various data
 floating-point numbers, and characters. The operations on these collections included determining the 
 maximum value, reversing the order of elements, and printing the collection contents. */
 
-
 #include<iostream>
 #include<vector>
 using namespace std;
